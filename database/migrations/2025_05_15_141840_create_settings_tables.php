d66ba780bf7ebb6654330bf5b8e18b3e157e2aa1
ahmad musleh <s12217787@stu.najah.edu>
2025-06-10 15:42:57 +0300

    finsh

--- database/migrations/2025_05_15_141840_create_settings_tables.php
+++ database/migrations/2025_05_15_141840_create_settings_tables.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->id('setting_id');
             $table->unsignedBigInteger('user_id')->unique();
-           // $table->enum('user_type', ['admin', 'artisan', 'job_owner']);
+            $table->enum('user_type', ['admin', 'artisan', 'job_owner']);
 
 
             $table->string('name');
